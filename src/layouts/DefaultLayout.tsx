3e22f10bf4570412be062c7dbe344a914ea466c5
Robin Bühler <git+r@obin.ch>
2022-07-15 12:27:58 +0000

    refactor: global styles out of layout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,4 +1,4 @@
-import { css, Global, Theme, ThemeProvider } from '@emotion/react';
+import { Global, Theme, ThemeProvider } from '@emotion/react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { ElementType, PropsWithChildren } from 'react';
 import { DefaultLayoutQuery } from '../../graphql-types';
@@ -8,6 +8,7 @@ import { Footer } from './default/Footer';
 import { Header } from './default/Header';
 import { Main } from './default/content/Main';
 import { defaultSyntaxHighlighting } from '../themes/defaultSyntaxHighlighting';
+import { defaultStyles } from '../themes/defaultStyles';
 
 const query = graphql`
   query DefaultLayout {
@@ -27,78 +28,6 @@ const query = graphql`
   }
 `;
 
-const globalStyles = (theme: Theme) => css`
-  html {
-    height: 100%;
-  }
-
-  body,
-  #___gatsby,
-  #gatsby-focus-wrapper,
-  main {
-    margin: 0;
-    font-size: 16px;
-    display: flex;
-    flex-direction: column;
-    flex-grow: 1;
-    min-height: 100%;
-    font-family: ${theme.fonts.paragraph};
-  }
-
-  header,
-  main,
-  footer,
-  #top-bar {
-    padding: 0 10%;
-
-    @media (max-width: ${theme.breakpoints.tiny}) {
-      padding: 0 2.5rem;
-    }
-
-    @media (min-width: ${theme.breakpoints.big}) {
-      padding: 0 15%;
-    }
-
-    @media (min-width: ${theme.breakpoints.huge}) {
-      padding: 0 20%;
-    }
-  }
-
-  header,
-  #top-bar,
-  footer {
-    font-family: ${theme.fonts.headings};
-
-    a {
-      text-decoration: none;
-      color: ${theme.colors.bright};
-    }
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    line-height: 1em;
-    font-family: ${theme.fonts.headings};
-  }
-
-  h1 {
-    font-size: 1.6rem;
-    margin-bottom: 1.6rem;
-  }
-
-  h2 {
-    margin-top: 2.5rem;
-  }
-
-  code {
-    font-family: ${theme.fonts.listings};
-  }
-`;
-
 type DefaultLayoutProps = PropsWithChildren<{
   theme?: Theme;
   subtitle?: string;
@@ -111,7 +40,7 @@ export function DefaultLayout({ children, theme, subtitle, contentWrapper: Conte
   return (
     <ThemeProvider theme={theme || defaultTheme}>
       <Document subtitle={subtitle} />
-      <Global styles={[globalStyles, defaultSyntaxHighlighting]} />
+      <Global styles={[defaultStyles, defaultSyntaxHighlighting]} />
       <Header />
       {ContentWrapper ? <ContentWrapper>{children}</ContentWrapper> : <Main>{children}</Main>}
       <Footer
---
+++ src/themes/defaultStyles.ts
@@ -0,0 +1,73 @@
+import { css, Theme } from '@emotion/react';
+
+export const defaultStyles = (theme: Theme) => css`
+  html {
+    height: 100%;
+  }
+
+  body,
+  #___gatsby,
+  #gatsby-focus-wrapper,
+  main {
+    margin: 0;
+    font-size: 16px;
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    min-height: 100%;
+    font-family: ${theme.fonts.paragraph};
+  }
+
+  header,
+  main,
+  footer,
+  #top-bar {
+    padding: 0 10%;
+
+    @media (max-width: ${theme.breakpoints.tiny}) {
+      padding: 0 2.5rem;
+    }
+
+    @media (min-width: ${theme.breakpoints.big}) {
+      padding: 0 15%;
+    }
+
+    @media (min-width: ${theme.breakpoints.huge}) {
+      padding: 0 20%;
+    }
+  }
+
+  header,
+  #top-bar,
+  footer {
+    font-family: ${theme.fonts.headings};
+
+    a {
+      text-decoration: none;
+      color: ${theme.colors.bright};
+    }
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1em;
+    font-family: ${theme.fonts.headings};
+  }
+
+  h1 {
+    font-size: 1.6rem;
+    margin-bottom: 1.6rem;
+  }
+
+  h2 {
+    margin-top: 2.5rem;
+  }
+
+  code {
+    font-family: ${theme.fonts.listings};
+  }
+`;
