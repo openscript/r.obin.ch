400459a85363c7b2ee4dd1292d6f87d90c963702
Robin <git+r@obin.ch>
2022-07-18 20:16:31 +0000

    fix: blog tag pages

--- src/templates/BlogTag.tsx
+++ src/templates/BlogTag.tsx
@@ -6,9 +6,9 @@ import { DefaultLayout } from '../layouts/DefaultLayout';
 export default function BlogTag({ data }: PageProps<BlogPageQuery>) {
   return (
     <DefaultLayout>
-      <h2>
+      <h1>
         <FormattedMessage id="page.blog.title" />
-      </h2>
+      </h1>
       {data.posts.nodes.map(post => (
         <h3>
           <Link to={post.fields?.path || ''}>{post.frontmatter?.title}</Link>
@@ -21,7 +21,7 @@ export default function BlogTag({ data }: PageProps<BlogPageQuery>) {
 export const query = graphql`
   query TagPage($tag: String!, $locale: String!, $limit: Int!, $skip: Int!) {
     posts: allMdx(
-      filter: { fields: { kind: { glob: "blog/**" }, locale: { eq: $locale } }, frontmatter: { tags: { glob: $tag } } }
+      filter: { fields: { kind: { glob: "blog/**" }, locale: { eq: $locale }, tags: { elemMatch: { slug: { glob: $tag } } } } }
       sort: { fields: frontmatter___publishedAt, order: DESC }
       limit: $limit
       skip: $skip
