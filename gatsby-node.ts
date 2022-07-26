62a5c920ba7bc8ce73cdbdcb43c489477ef8fc9e
Robin <git+r@obin.ch>
2022-07-26 17:31:09 +0000

    refactor: rename create blog pages action

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,6 +1,6 @@
 import { GatsbyNode } from 'gatsby';
 import { createBlogListingPages } from './src/gatsby/createPages/createBlogListingPages';
-import { createBlogPages } from './src/gatsby/createPages/createBlogPostPages';
+import { createBlogPages } from './src/gatsby/createPages/createBlogPages';
 import { createBlogTagPages } from './src/gatsby/createPages/createBlogTagPages';
 import { createGenericPages } from './src/gatsby/createPages/createGenericPages';
 import { createProjectListingPages } from './src/gatsby/createPages/createProjectListingPages';
