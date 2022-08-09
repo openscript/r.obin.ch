116c92f3dc52ad99617241219cd7e06475a39f5b
Robin <git+r@obin.ch>
2022-08-09 20:21:06 +0000

    feat: enhance share button

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -1,12 +1,24 @@
 import { FormattedMessage } from 'react-intl';
 
-export function ShareButton() {
+type ShareButtonProps = {
+  text?: string;
+  title?: string;
+  url?: string;
+};
+
+export function ShareButton({ text, title, url }: ShareButtonProps) {
   if (typeof window === 'undefined' || !window.navigator || 'share' in window.navigator === false) {
     return null;
   }
 
-  const onShare = () => {
-    navigator.share();
+  const shareData: ShareData = {
+    title: title || document.title,
+    text,
+    url: url || document.location.href,
+  };
+
+  const onShare = async () => {
+    await navigator.share(shareData);
   };
 
   return (
