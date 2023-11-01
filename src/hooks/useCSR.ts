a010bc28ea76e2910cb8f3f1d6c41dae3b8f9b06
Robin Bühler <git+r@obin.ch>
2023-11-01 06:25:59 +0000

    fix: footer hydration

---
+++ src/hooks/useCSR.ts
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react';
+
+export function useCSR() {
+  const [isCSR, setCSR] = useState(false);
+
+  useEffect(() => {
+    setCSR(true);
+  }, []);
+
+  return isCSR;
+}
--- src/layouts/default/Footer.tsx
+++ src/layouts/default/Footer.tsx
@@ -2,6 +2,7 @@ import { css, Theme } from '@emotion/react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { ReboundSection } from 'react-section-dividers';
 import FooterNavigation from './FooterNavigation';
+import { useCSR } from '../../hooks/useCSR';
 
 const footerStyle = (theme: Theme) => css`
   display: flex;
@@ -24,13 +25,29 @@ type FooterProps = {
 };
 
 export function Footer({ author, project, version, buildTime }: FooterProps) {
+  const isCSR = useCSR();
   const intl = useIntl();
+  const formattedBuildTime = {
+    date: intl.formatDate(buildTime, { timeZone: 'UTC' }),
+    time: intl.formatTime(buildTime, { timeZone: 'UTC' }),
+  };
+
+  if (isCSR) {
+    formattedBuildTime.date = intl.formatDate(buildTime);
+    formattedBuildTime.time = intl.formatTime(buildTime);
+  }
+
   return (
     <ReboundSection as="footer" position="top" flip="both" rebound={80} height={30} css={footerStyle}>
       <div css={pageInfoStyles}>
         <FormattedMessage
           id="pageInfoText"
-          values={{ project, author, version, time: intl.formatTime(buildTime), date: intl.formatDate(buildTime) }}
+          values={{
+            project,
+            author,
+            version,
+            ...formattedBuildTime,
+          }}
         />
       </div>
       <FooterNavigation />
