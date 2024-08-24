8edd3bf7d68e7d2f6700e7dd0a649a9edf246b01
Robin Bühler <git+r@obin.ch>
2024-08-24 20:09:58 +0000

    feat: add icons

---
+++ astro.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'astro/config';
+import unpluginFavicons from "@anolilab/unplugin-favicons/vite";
+import { C } from './src/configuration';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [
+      unpluginFavicons({
+        logo: "res/brand/icon.png",
+        inject: true,
+        favicons: {
+          theme_color: C.SETTINGS.BROWSER.THEME_COLOR,
+          icons: {
+            android: true,
+            appleIcon: true,
+            favicons: true,
+            windows: true,
+            yandex: true,
+            appleStartup: false
+          }
+        },
+      })
+    ]
+  }
+});
---
+++ src/configuration.ts
@@ -0,0 +1,31 @@
+export const C = {
+  // Key is the locale slug, value is the locale code
+  LOCALES: { 'en': 'en-US', 'de': 'de-CH' },
+  // Default locale referencing one of the keys in LOCALES
+  DEFAULT_LOCALE: 'en' as const,
+  // Static messages for each locale
+  MESSAGES: {
+    'en': {
+      'language': 'English',
+    },
+    'de': {
+      'language': 'Deutsch',
+    }
+  } satisfies { [key: string]: {[key: string]: string }},
+  // Various settings
+  SETTINGS: {
+    BROWSER: {
+      THEME_COLOR: "#663399",
+    },
+    REMOTE: {
+      BASE_URL: 'https://r.obin.ch',
+    },
+    BLOG: {
+      PAGE_SIZE: 20,
+    }
+  },
+} as const;
+
+// Configuration helpers
+export type Locale = keyof typeof C.LOCALES;
+export const localeSlugs = Object.keys(C.LOCALES) as Locale[];
